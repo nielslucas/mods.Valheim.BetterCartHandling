26e3dc926794a8c91985d594222d0058150f26a7
Niels Lucas <niels_lucas@hotmail.com>
2023-04-08 14:17:22 +0200

    first commit

---
+++ BetterCartHandling/BetterCartHandling.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BetterCartHandling
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    [BepInProcess("valheim.exe")]
+    public class BetterCartHandling : BaseUnityPlugin
+    {
+        private const string modGUID = "ForshenMod.BetterCartHandling";
+        private const string modName = "BetterCartHandling";
+        private const string modVersion = "0.0.1";
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public void Awake()
+        {
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ BetterCartHandling/BetterCartHandling_Patch.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BetterCartHandling
+{
+    [HarmonyPatch(typeof(Vagon), "Awake")]
+    public class BetterCartHandling_Patch
+    {
+        [HarmonyPrefix]
+        public static void SetCartPatch(ref float ___m_itemWeightMassFactor)
+        {
+            ___m_itemWeightMassFactor = 0;
+        }
+    }
+}
